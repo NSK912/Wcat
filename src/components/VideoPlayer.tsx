2a3f0a467917b8e722452758e6d9ac831c69433f
Nat <nang105447@outlook.com>
2026-08-27 20:28:04 +0700

    Add files via upload

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1118,7 +1118,7 @@ export const VideoPlayer: React.FC<VideoPlayerProps> = ({
           </div>
           <div className="text-slate-500 text-xs font-medium font-mono">
             <span className="font-bold mr-2">NSK App</span>
-            ver 3.5.0.5
+            ver 3.5.0.6
           </div>
         </div>
       ) : loadError ? (
